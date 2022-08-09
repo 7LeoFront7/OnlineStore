291e405be04947c57898d2d58f8d2336a61f9969
Maxim Nazarov <xonskyfilms@gmail.com>
2022-08-09 10:29:20 +0300

    Create server and DB

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-let lf = 10;
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+
+const start = async () => {
+        try {
+                await sequelize.authenticate()
+                await sequelize.sync()
+                app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        } catch (error) {
+                console.log(error)
+        }
+}
+
+start()
+
