b631482ac9ba0f49c192c86f638a0939cba013ce
Maxim Nazarov <xonskyfilms@gmail.com>
2022-08-09 12:26:31 +0300

    Backend part

---
+++ server/controllers/brandController.js
@@ -0,0 +1,13 @@
+class BrandController {
+
+        async create(req, res){
+
+        }
+
+        async getAll(req, res){
+
+        }
+
+}
+
+module.exports = new BrandController()
---
+++ server/controllers/deviceController.js
@@ -0,0 +1,17 @@
+class DeviceController {
+
+        async create(req, res){
+
+        }
+
+        async getAll(req, res){
+
+        }
+
+        async getOne(req, res){
+
+        }
+
+}
+
+module.exports = new DeviceController()
